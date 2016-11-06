67f2066a576b3b521681599b80eb63130d6e8203
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-06 17:30:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const episodes = [
+var episodes = [
   { id: 's06e01', title: 'The Red Woman' },
   { id: 's06e02', title: 'Home' },
   { id: 's06e03', title: 'Oathbreaker' },
@@ -10,4 +10,32 @@ const episodes = [
   { id: 's06e09', title: 'Battle of the Bastards' },
 ];
 
-const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+var finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+
+function addToPlaylist(playlist, episode){
+  return playlist.concat(episode)
+}
+
+function removeFromPlaylist(playlist, episode){
+   var newPlaylist = playlist.slice();
+   var index = playlist.indexOf(episode);
+   newPlaylist.splice(index, 1);
+   return newPlaylist;
+}
+
+function getNextEpisodeInPlaylist(playlist){
+  return playlist[0]
+}
+
+function bingeWatch(playlist){
+  playlist = playlist.slice();
+  while (playlist.length > 0){
+    playlist.splice(0, 1)
+    return bingeWatch(playlist)
+  }
+  
+  return 'Please let there be more!'
+}
+
+
